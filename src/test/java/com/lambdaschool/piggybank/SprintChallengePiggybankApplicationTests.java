ba0bd04625d574b1bcea83d528451a5d509ffce6
Oscar Figueroa <Osc.Fig96@gmail.com>
2021-02-12 07:21:17 -0800

    Initial setup

--- src/test/java/com/lambdaschool/piggybank/SprintChallengePiggybankApplicationTests.java
+++ src/test/java/com/lambdaschool/piggybank/SprintChallengePiggybankApplicationTests.java
@@ -4,8 +4,10 @@ import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
 @SpringBootTest
-public class PiggybankApplicationTest {
+class SprintChallengePiggybankApplicationTests {
+
     @Test
     void contextLoads() {
     }
+
 }
