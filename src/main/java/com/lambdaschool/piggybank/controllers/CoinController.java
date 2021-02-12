a6e465d7776a0f819042cfeccd06a0dc9b31e065
Oscar Figueroa <Osc.Fig96@gmail.com>
2021-02-12 10:33:03 -0800

    All done

---
+++ src/main/java/com/lambdaschool/piggybank/controllers/CoinController.java
@@ -0,0 +1,48 @@
+package com.lambdaschool.piggybank.controllers;
+
+import com.lambdaschool.piggybank.models.Coin;
+import com.lambdaschool.piggybank.repositories.CoinRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.ArrayList;
+import java.util.List;
+
+@RestController
+public class CoinController
+{
+    private List<Coin> findCoins(List<Coin> myList, CheckCoin tester)
+    {
+        List<Coin> tempList = new ArrayList<>();
+
+        for (Coin c : myList)
+        {
+            if (tester.test(c))
+            {
+                tempList.add(c);
+            }
+        }
+        return tempList;
+    }
+
+    @Autowired
+    CoinRepository crepos;
+
+    // http://localhost:2019/total
+    @GetMapping(value = "/total", produces = {"application/json"})
+    public ResponseEntity<?> totalAmount(){
+        List<Coin> coinList = new ArrayList<>();
+        crepos.findAll().iterator().forEachRemaining(coinList::add);
+
+        double total = 0;
+        for (Coin c : coinList){
+            total += (c.getValue() * c.getQuantity());
+            System.out.println(c.getQuantity() + " " + (c.getQuantity() > 1 ? c.getNamePlural() : c.getName()));
+        }
+        System.out.println("The piggybank is holding " + total);
+        return new ResponseEntity<>(coinList, HttpStatus.OK);
+    }
+}
---
+++ src/main/java/com/lambdaschool/piggybank/models/Coin.java
@@ -0,0 +1,79 @@
+package com.lambdaschool.piggybank.models;
+
+import javax.persistence.*;
+
+@Entity
+@Table(name = "Coins")
+public class Coin
+{
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    private long coinid;
+    private int quantity;
+    private double value;
+    private String name;
+    private String nameplural;
+
+    public Coin()
+    {
+        // required JPA
+    }
+
+    public Coin(int quantity, double value, String name, String nameplural) {
+        this.quantity = quantity;
+        this.value = value;
+        this.name = name;
+        this.nameplural = nameplural;
+    }
+
+    public long getCoinId() {
+        return coinid;
+    }
+
+    public void setCoinId(long coinid) {
+        this.coinid = coinid;
+    }
+
+    public int getQuantity() {
+        return quantity;
+    }
+
+    public void setQuantity(int quantity) {
+        this.quantity = quantity;
+    }
+
+    public double getValue() {
+        return value;
+    }
+
+    public void setValue(double value) {
+        this.value = value;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getNamePlural() {
+        return nameplural;
+    }
+
+    public void setNamePlural(String nameplural) {
+        this.nameplural = nameplural;
+    }
+
+    @Override
+    public String toString() {
+        return "Coin{" +
+                "coinid=" + coinid +
+                ", quantity=" + quantity +
+                ", value=" + value +
+                ", name='" + name + '\'' +
+                ", nameplural='" + nameplural + '\'' +
+                '}';
+    }
+}
